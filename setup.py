14a9e91af8d2be27e436839bc1cebe5370478f64
Matthew Brett <matthew.brett@gmail.com>
2015-10-23 12:59:58 -0400

    NF: add Windows .bat file wrappers
    Using myscripter project.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 ''' Installation script for nb2plots package '''
+import os
+from os.path import join as pjoin, split as psplit, splitext
 import sys
 
 # For some commands, use setuptools.
@@ -17,9 +19,58 @@ extra_setup_kwargs = ({} if 'setuptools' not in sys.modules else
                       dict(install_requires=['six', 'sphinx>=1.1.3']))
 
 
+# See: https://github.com/matthew-brett/myscripter
+from distutils.command.install_scripts import install_scripts
+from distutils import log
+
+BAT_TEMPLATE = \
+r"""@echo off
+REM wrapper to use shebang first line of {FNAME}
+set mypath=%~dp0
+set pyscript="%mypath%{FNAME}"
+set /p line1=<%pyscript%
+if "%line1:~0,2%" == "#!" (goto :goodstart)
+echo First line of %pyscript% does not start with "#!"
+exit /b 1
+:goodstart
+set py_exe=%line1:~2%
+call "%py_exe%" %pyscript% %*
+"""
+
+class my_install_scripts(install_scripts):
+    """ Install .bat wrapper for scripts on Windows """
+    def run(self):
+        install_scripts.run(self)
+        if not os.name == "nt":
+            return
+        for filepath in self.get_outputs():
+            # If we can find an executable name in the #! top line of the
+            # script file, make .bat wrapper for script.
+            with open(filepath, 'rt') as fobj:
+                first_line = fobj.readline()
+            if not (first_line.startswith('#!') and
+                    'python' in first_line.lower()):
+                log.info("No #!python executable found, skipping .bat "
+                            "wrapper")
+                continue
+            pth, fname = psplit(filepath)
+            froot, ext = splitext(fname)
+            bat_file = pjoin(pth, froot + '.bat')
+            bat_contents = BAT_TEMPLATE.replace('{FNAME}', fname)
+            log.info("Making %s wrapper for %s" % (bat_file, filepath))
+            if self.dry_run:
+                continue
+            with open(bat_file, 'wt') as fobj:
+                fobj.write(bat_contents)
+
+
+CMDCLASS = versioneer.get_cmdclass()
+CMDCLASS['install_scripts'] = my_install_scripts
+
+
 setup(name='nb2plots',
       version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
+      cmdclass=CMDCLASS,
       description='Converting between ipython notebooks and sphinx docs',
       author='Matthew Brett',
       author_email='matthew.brett@gmail.com',
