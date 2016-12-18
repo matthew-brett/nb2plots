dfaaa0dee4c22f38dc67dcbc44c0cbe8e3c77b09
Matthew Brett <matthew.brett@gmail.com>
2016-12-17 21:43:20 -0500

    RF: refactor notebook roles into classes
    Ready for code role.

--- nb2plots/runroles.py
+++ nb2plots/runroles.py
@@ -27,65 +27,71 @@ class runrole_reference(nodes.reference):
     """Node for references to built runnable, similar to pending_xref."""
 
 
-def clearnotebook(name, rawtext, text, lineno, inliner, options={},
-                  content=[]):
-    """ Role for building and linking to Jupyter notebooks
+class ClearNotebookRunRole(object):
+    """ Role builder for not-evaluated notebook """
 
-    Parameters
-    ----------
-    name : str
-        The role name used in the document.
-    rawtext : str
-        The entire markup snippet, including the role markup.
-    text : str
-        The text marked with the role.
-    lineno : int
-        The line number where `rawtext` appears in the input.
-    inliner : object
-        The inliner instance that called us.
-    options : dict, optional
-        Directive options for customization.
-    content : content, optional
-        The directive content for customization.
+    evaluate = False
+    default_text = 'Download this page as a Jupyter notebook'
+    default_extension = '.ipynb'
 
-    Returns
-    -------
-    nodes : list
-        list of nodes to insert into the document. Can be empty.
-    messages : list
-        list of system messages. Can be empty.
-    """
-    # process options
-    # http://docutils.sourceforge.net/docs/howto/rst-roles.html
-    evaluate = options.pop('evaluate', False)
-    set_classes(options)
-    # Get objects from context
-    env = inliner.document.settings.env
-    # Get title and link
-    text = utils.unescape(text)
-    if text.strip() == '.':
-        text = 'Download this page as a Jupyter notebook'
-    has_nb_fname, title, nb_fname = split_explicit_title(text)
-    if not has_nb_fname:
-        nb_fname = env.docname + '.ipynb'
-    refnode = runrole_reference(rawtext, title, reftype=name)
-    # We may need the line number for warnings
-    set_role_source_info(inliner, lineno, refnode)
-    refnode['reftarget'] = nb_fname
-    # we also need the source document
-    refnode['refdoc'] = env.docname
-    refnode['evaluate'] = evaluate
-    # result_nodes allow further modification of return values
-    return [refnode], []
+    def __call__(self, name, rawtext, text, lineno, inliner, options={},
+                    content=[]):
+        """ Role for building and linking to built code files
 
-clearnotebook.options = {'evaluate': directives.flag}
+        Parameters
+        ----------
+        name : str
+            The role name used in the document.
+        rawtext : str
+            The entire markup snippet, including the role markup.
+        text : str
+            The text marked with the role.
+        lineno : int
+            The line number where `rawtext` appears in the input.
+        inliner : object
+            The inliner instance that called us.
+        options : dict, optional
+            Directive options for customization.
+        content : content, optional
+            The directive content for customization.
+
+        Returns
+        -------
+        nodes : list
+            list of nodes to insert into the document. Can be empty.
+        messages : list
+            list of system messages. Can be empty.
+        """
+        # process options
+        # http://docutils.sourceforge.net/docs/howto/rst-roles.html
+        set_classes(options)
+        # Get objects from context
+        env = inliner.document.settings.env
+        # Get title and link
+        text = utils.unescape(text)
+        text = self.default_text if text.strip() == '.' else text
+        has_fname, title, fname = split_explicit_title(text)
+        if not has_fname:
+            fname = env.docname + self.default_extension
+        refnode = runrole_reference(rawtext, title, reftype=name)
+        # We may need the line number for warnings
+        set_role_source_info(inliner, lineno, refnode)
+        refnode['reftarget'] = fname
+        # we also need the source document
+        refnode['refdoc'] = env.docname
+        refnode['evaluate'] = self.evaluate
+        # result_nodes allow further modification of return values
+        return [refnode], []
 
 
-def fullnotebook(name, rawtext, text, lineno, inliner, options={}, content=[]):
-    """" Role to force evaluation of notebook when building """
-    options['evaluate'] = True
-    return clearnotebook(name, rawtext, text, lineno, inliner, options=options,
-                         content=content)
+class FullNotebookRunRole(ClearNotebookRunRole):
+    """ Role builder for evaluated notebook """
+
+    evaluate = True
+
+
+clearnotebook = ClearNotebookRunRole()
+fullnotebook = FullNotebookRunRole()
 
 
 def collect_notebooks(app, doctree, fromdocname):
