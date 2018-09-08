af0b345c85b0f76714b66ce055bfd14d91e97d4c
Matthew Brett <matthew.brett@gmail.com>
2018-09-08 08:28:50 +0100

    BF: update for new nbconvert version
    nbconvert 5.4.0 introduces a new `language_info` node in `metadata` of
    notebook.  Remove to test against expected notebook.

--- nb2plots/testing/nbtesters.py
+++ nb2plots/testing/nbtesters.py
@@ -11,4 +11,8 @@ def assert_nb_equiv(ipynb, expected):
     # It does not appear to be possible to request specific minor versions of
     # the Notebook format.
     expected_nb['nbformat_minor'] = actual_nb['nbformat_minor']
+    # 'language_info' key seems to have arrived in metadata as a result of
+    # nbconvert 5.3.1 -> 5.4.0 (5.4.0 released September 7 2018).  Previously
+    # it was empty.
+    actual_nb['metadata'].pop('language_info', None)
     assert actual_nb == expected_nb
