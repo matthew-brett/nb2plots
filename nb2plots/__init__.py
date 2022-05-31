903481dd3e8a25e0d66bcafaa0f53cb1c7a6549e
Matthew Brett <matthew.brett@gmail.com>
2022-05-31 12:49:09 +0100

    Update versioneer

--- nb2plots/__init__.py
+++ nb2plots/__init__.py
@@ -1,8 +1,7 @@
 # nb2plots package
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 from . import nbplots
 from . import runroles
