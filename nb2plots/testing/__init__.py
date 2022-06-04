b575e85f833a0d26223c55cb3143d0f948b1fef6
Matthew Brett <matthew.brett@gmail.com>
2022-06-04 12:47:03 +0100

    Drop use of LooseVersion, update requirements

--- nb2plots/testing/__init__.py
+++ nb2plots/testing/__init__.py
@@ -15,12 +15,7 @@ DATA_PATH = abspath(pjoin(
 
 
 def setup_test():
-    """ Set numpy print options to "legacy" for new versions of numpy
-    """
-    from distutils.version import LooseVersion
-
-    if LooseVersion(np.__version__) >= LooseVersion('1.14'):
-        np.set_printoptions(legacy="1.13")
+    np.set_printoptions(legacy="1.13")
 
 
 class PlotsBuilder(SourcesBuilder):
