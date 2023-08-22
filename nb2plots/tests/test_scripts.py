fea1d1eea5ec74f5d11fa4dd7ba1c211d08f09e5
Matthew Brett <matthew.brett@gmail.com>
2023-08-22 10:18:49 +0100

    Fix for broken import in nb2plots
    Closes gh-31.

--- nb2plots/tests/test_scripts.py
+++ nb2plots/tests/test_scripts.py
@@ -5,19 +5,20 @@
 Test running scripts
 """
 
-from os.path import (join as pjoin, exists)
-from glob import glob
+from pathlib import Path
 import re
 
 from scripttester import ScriptTester
 
-from nb2plots.testing import DATA_PATH
-from nb2plots.testing.convutils import fcontents, unsmart, unsmart_nb
+from nb2plots.testing.convutils import unsmart, unsmart_nb
 from nb2plots.testing.nbtesters import assert_nb_equiv
 
 
 runner = ScriptTester('nb2plots', win_bin_ext='.bat')
 run_command = runner.run_command
+
+TESTS_PATH = Path(__file__).parent
+RST_MD_PATH = TESTS_PATH / 'rst_md_files'
 
 
 def script_test(func):
@@ -29,16 +30,15 @@ def script_test(func):
 @script_test
 def test_rst2md():
     # test rst2md script over all .rst files checking against .md files
-    for rst_fname in glob(pjoin(DATA_PATH, '*.rst')):
-        md_fname = rst_fname[:-3] + 'md'
-        with open(md_fname, 'rb') as fobj:
-            expected_md = fobj.read()
+    for rst_path in RST_MD_PATH.glob('*.rst'):
+        md_path = rst_path.with_suffix('.md')
+        expected_md = md_path.read_bytes()
         # Skip files containing text "skip".  These are files for which the
         # source ReST is not valid in plain docutils, such as those containing
         # Sphinx directives and roles.
         if expected_md.strip() == b'skip':
             continue
-        cmd = ['rst2md', rst_fname]
+        cmd = ['rst2md', str(rst_path)]
         code, stdout, stderr = run_command(cmd)
         assert stdout == expected_md
 
@@ -47,13 +47,13 @@ def test_rst2md():
 def test_sphinx2md():
     # test sphinx2md script over all .rst files checking against .smd / .md
     # files
-    for rst_fname in glob(pjoin(DATA_PATH, '*.rst')):
+    for rst_path in RST_MD_PATH.glob('*.rst'):
         # Try .smd filename first, otherwise ordinary .md
-        md_fname = rst_fname[:-3] + 'smd'
-        if not exists(md_fname):
-            md_fname = rst_fname[:-3] + 'md'
-        expected_md = fcontents(md_fname)
-        cmd = ['sphinx2md', rst_fname]
+        md_path = rst_path.with_suffix('.smd')
+        if not md_path.is_file():
+            md_path = rst_path.with_suffix('.md')
+        expected_md = md_path.read_bytes()
+        cmd = ['sphinx2md', str(rst_path)]
         code, stdout, stderr = run_command(cmd)
         assert (unsmart(stdout.decode('utf-8')) ==
                 expected_md.decode('utf-8'))
@@ -62,10 +62,10 @@ def test_sphinx2md():
 @script_test
 def test_sphinx2nb():
     # test sphinx2nb script over all .rst files checking against .ipynb files
-    for rst_fname in glob(pjoin(DATA_PATH, '*.rst')):
-        nb_fname = rst_fname[:-3] + 'ipynb'
-        expected = fcontents(nb_fname, 't')
-        cmd = ['sphinx2nb', rst_fname]
+    for rst_path in RST_MD_PATH.glob('*.rst'):
+        nb_path = rst_path.with_suffix('.ipynb')
+        expected = nb_path.read_text()
+        cmd = ['sphinx2nb', str(rst_path)]
         code, stdout, stderr = run_command(cmd)
         assert_nb_equiv(unsmart_nb(stdout.decode('utf-8')),
                         expected)
@@ -74,10 +74,10 @@ def test_sphinx2nb():
 @script_test
 def test_sphinx2py():
     # test sphinx2py script over all .rst files checking against .ipynb files
-    for rst_fname in glob(pjoin(DATA_PATH, '*.rst')):
-        py_fname = rst_fname[:-3] + 'py'
-        expected = fcontents(py_fname, 'b')
-        cmd = ['sphinx2py', rst_fname]
+    for rst_path in RST_MD_PATH.glob('*.rst'):
+        py_path = rst_path.with_suffix('.py')
+        expected = py_path.read_bytes()
+        cmd = ['sphinx2py', str(rst_path)]
         code, stdout, stderr = run_command(cmd)
         assert (unsmart(stdout.decode('utf-8')) ==
                 expected.decode('utf-8'))
@@ -85,8 +85,8 @@ def test_sphinx2py():
 
 @script_test
 def test_sphinx2pxml():
-    rst_fname = pjoin(DATA_PATH, 'sect_text.rst')
-    cmd = ['sphinx2pxml', rst_fname]
+    rst_path = RST_MD_PATH / 'sect_text.rst'
+    cmd = ['sphinx2pxml', str(rst_path)]
     code, stdout, stderr = run_command(cmd)
     pattern = r"""<document source=".*?">
     <section ids="a-section" names="a\\ section">
@@ -99,3 +99,16 @@ def test_sphinx2pxml():
             ."""
     output = stdout.decode('utf-8')
     assert re.match(pattern, output)
+
+
+@script_test
+def test_nb2plots():
+    fname = 'example_notebook'
+    data_path = TESTS_PATH / 'data'
+    nb_path = data_path / (fname + '.ipynb')
+    rst_path = data_path / 'converted_example.rst'
+    expected = rst_path.read_text()
+    cmd = ['nb2plots', str(nb_path)]
+    code, stdout, stderr = run_command(cmd)
+    output = stdout.decode('utf-8')
+    assert output.strip() == expected.strip()
